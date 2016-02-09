014aa64ce1464514871333c1dbfd4ac0b74a1dbb
Jehy <fate@jehy.ru>
2016-02-09 11:33:42 +0300

    refactoring

--- arduino-esp8266-mhz-19-serial.ino
+++ arduino-esp8266-mhz-19-serial.ino
@@ -1,112 +1,118 @@
 #include "SoftwareSerial.h"
 #include "DHT.h"
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
+#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <Wire.h>
 #include "wifi-utils.h"
 #include "wifi-creds.h"
 
 
+#define INTERVAL 5000
+#define DHT_PIN D4
+#define DHT_VERSION DHT11
+#define MH_Z19_RX D7
+#define MH_Z19_TX D6
+
 char macStr[20];
-long previousMillis = 0;        // will store last time LED was updated
-long interval = 5000;           // interval at which to blink (milliseconds)
+long previousMillis = 0;
 
-DHT dht(D4, DHT11);           // Объявление переменной класса dht11
+DHT dht(DHT_PIN, DHT_VERSION);//define temperature and humidity sensor
 
 
-SoftwareSerial mySerial(D7,D6); // RX, TX сенсора
+SoftwareSerial mySerial(MH_Z19_RX, MH_Z19_TX); // define MH-Z19 using
 
-byte cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
-// волшебная комманда - смотри мануал
-char response[9]; // здесь будет ответ
 
 
 void setup() {
-Serial.begin(115200); //это наш монитор
-mySerial.begin(9600); //а это датчик MH-Z19(14)
-dht.begin();
+  Serial.begin(115200); // Init console
+  mySerial.begin(9600); //Init sensor MH-Z19(14)
+  dht.begin();
 
   if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present");
     // don't continue:
     while (true)
-    delay(10000);
+      delay(10000);
   }
 
-  
- // attempt to connect to Wifi network:
+
+  // attempt to connect to Wifi network:
   while ( status != WL_CONNECTED) {
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network:    
+    // Connect to WPA/WPA2 network:
     status = WiFi.begin(ssid, pass);
     delay(10000);
   }
-   
+
   // you're connected now, so print out the data:
   Serial.print("You're connected to the network");
   printCurrentNet();
   printWifiData();
 
+  //get MAC address
+  byte mac[6];
+  WiFi.macAddress(mac);
+  sprintf(macStr, "%02x:%02x:%02x:%02x:%02x:%02x", mac[5], mac[4], mac[3], mac[2], mac[1], mac[0]);
   
-    byte mac[6];  
-    WiFi.macAddress(mac);
-    sprintf(macStr,"%02x:%02x:%02x:%02x:%02x:%02x",mac[5],mac[4],mac[3],mac[2],mac[1],mac[0]);
-delay(2000);
+  delay(10000);//to init and heat sensors
 }
 
+int ReadCO2()
+{
+
+  byte cmd[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
+  // command to ask for data
+  char response[9]; // for answer
+
+  mySerial.write(cmd, 9); //request PPM CO2
+  mySerial.readBytes(response, 9);
+  int responseHigh = (int) response[2];
+  int responseLow = (int) response[3];
+  int ppm = (256 * responseHigh) + responseLow;
+  return ppm;
+}
 void loop()
 {
-  
-    unsigned long currentMillis = millis();
- 
-  if(currentMillis - previousMillis < interval) 
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis < INTERVAL)
     return;
-   // save the last time you blinked the LED 
-   previousMillis = currentMillis;   
-  //dns.processNextRequest();  
+  previousMillis = currentMillis;
+  //dns.processNextRequest();
   //server.handleClient();
-  Serial.println("loop started");  
+  Serial.println("loop started");
   Serial.println("reading data");
-mySerial.write(cmd,9);//запрос PPM CO2
-mySerial.readBytes(response, 9);
-int responseHigh = (int) response[2];
-int responseLow = (int) response[3];
-int ppm = (256*responseHigh)+responseLow;
-// ну и по мануалу из ответа считаем PPM
+  int ppm = ReadCO2();
 
-Serial.println("PPM = "+String(ppm));
+  Serial.println("PPM = " + String(ppm));
 
+  int mem = ESP.getFreeHeap();
+  Serial.println("Free RAM: " + String(mem));
 
+  float h = dht.readHumidity();
 
-float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
-// Считываем температуру
-
-float t = dht.readTemperature();
-
-  // Выводим показания влажности и температуры
   Serial.print("Humidity = ");
   Serial.print(h, 1);
   Serial.print(", Temp = ");
-  Serial.println(t,1);
-  
-Serial.println("");
+  Serial.println(t, 1);
 
-
-  wifiCheckReconnect(ssid,pass);
-  Serial.println("\nCurrent net:");
+  wifiCheckReconnect(ssid, pass);
+  Serial.println("Current net:");
   printCurrentNet();
-  Serial.println("\nStarting connection to server...");
+  Serial.println("Starting connection to server...");
   // if you get a connection, report back via serial:
   wdt_reset();
-  if (client.connect("co2.jehy.ru", 80)) 
+  if (client.connect("co2.jehy.ru", 80))
   {
-    Serial.println("connected to server");  
+    Serial.println("connected to server");
     // Make a HTTP request:
-    client.println("GET /send.php?data={\"id\":1,\"temp\":"+String(t)+",\"humidity\":"+String(h)+",\"ppm\":"+String((int)ppm)+
-    ",\"mac\":\""+String(macStr)+"\",\"SSID\":\""+WiFi.SSID()+"\"} HTTP/1.1");
+    client.println("GET /send.php?data={\"id\":1,\"temp\":" + String(t) + ",\"humidity\":" + String(h) + ",\"ppm\":" + String((int)ppm) +
+                   ",\"mac\":\"" + String(macStr) + "\",\"FreeRam\":\"" + String(mem) + "\",\"SSID\":\"" + WiFi.SSID() + "\"} HTTP/1.1");
     client.println("Host: co2.jehy.ru");
     client.println("Connection: close");
     client.println();
@@ -114,5 +120,6 @@ Serial.println("");
     Serial.println("Request sent");
   }
   Serial.println("loop finished");
-delay(2000);
+  Serial.println("");
+  delay(2000);
 }
