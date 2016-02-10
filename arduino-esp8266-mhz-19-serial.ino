233ae2af9eafe307420d8c58a014f94c870cac55
Jehy <fate@jehy.ru>
2016-02-10 14:37:00 +0300

    add max attempts for wifi connections and self reset, clean up code

--- arduino-esp8266-mhz-19-serial.ino
+++ arduino-esp8266-mhz-19-serial.ino
@@ -2,7 +2,6 @@
 #include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 #include <Wire.h>
 #include <ArduinoJson.h>
 #include <LiquidCrystal_I2C.h>
@@ -14,10 +13,13 @@
 #define DHT_VERSION DHT11
 #define MH_Z19_RX D7
 #define MH_Z19_TX D6
+#define WIFI_MAX_ATTEMPTS_INIT 5 //set to 0 for unlimited, do not use more then 65535
+#define WIFI_MAX_ATTEMPTS_SEND 1 //set to 0 for unlimited, do not use more then 65535
+#define MAX_DATA_ERRORS 15 //max of errors, reset after them
 
 char macStr[20];
 long previousMillis = 0;
-bool sentOk = false;
+int errorCount = 0;
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2); // display address and size
 DHT dht(DHT_PIN, DHT_VERSION);//define temperature and humidity sensor
@@ -25,62 +27,7 @@ DHT dht(DHT_PIN, DHT_VERSION);//define temperature and humidity sensor
 
 SoftwareSerial mySerial(MH_Z19_RX, MH_Z19_TX); // define MH-Z19 using
 
-
-
-void setup() {
-  Serial.begin(115200); // Init console
-  Serial.println("Setup started");
-  mySerial.begin(9600); //Init sensor MH-Z19(14)
-  dht.begin();
-
-  lcd.begin();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Initializing...");
-
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-    // don't continue:
-    while (true)
-      delay(10000);
-  }
-
-
-  // attempt to connect to Wifi network:
-  while ( status != WL_CONNECTED) {
-    lcd.setCursor(0, 0);
-    lcd.print("Connecting...");
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-    delay(10000);
-  }
-
-  //set google DNS
-  IPAddress googleDNS(8, 8, 8, 8);
-  WiFi.config(WiFi.localIP(), WiFi.gatewayIP(), WiFi.subnetMask(), googleDNS);
-
-  // you're connected now, so print out the data:
-  Serial.print("You're connected to the network");
-  printCurrentNet();
-  printWifiData();
-
-  //get MAC address
-  byte mac[6];
-  WiFi.macAddress(mac);
-  sprintf(macStr, "%02x:%02x:%02x:%02x:%02x:%02x", mac[5], mac[4], mac[3], mac[2], mac[1], mac[0]);
-
-  Serial.println("Waiting for sensors to init");
-  lcd.setCursor(0, 0);
-  lcd.print("Heating...");
-  delay(10000);
-  Serial.println("Setup finished");
-  Serial.println("");
-  lcd.setCursor(0, 0);
-  lcd.print("Starting...");
-  lcd.noBacklight();
-}
+void(* resetFunc) (void) = 0; //declare reset function @ address 0
 
 int readCO2()
 {
@@ -97,7 +44,7 @@ int readCO2()
   return ppm;
 }
 
-bool sendData(String data)
+bool sendData(JsonObject& root)
 {
   Serial.println("Starting connection to server...");
   bool res = false;
@@ -115,6 +62,12 @@ bool sendData(String data)
     client.println("Content-Type: application/x-www-form-urlencoded;");
 
     client.print("Content-Length: ");
+    String data;
+    root.printTo(data);
+
+    //Serial.println("data to send:");
+    //Serial.println(data);
+    data = "data=" + data;
     client.println(data.length());
     client.println();
     client.println(data);
@@ -139,32 +92,124 @@ bool sendData(String data)
   }
 }
 
+void lcdPrint(int statNum, String str)
+{
+  if (statNum == 1)
+    lcd.setCursor(0, 0);
+  else if (statNum == 2)
+    lcd.setCursor(8, 0);
+  else if (statNum == 3)
+    lcd.setCursor(0, 1);
+  else if (statNum == 4)
+    lcd.setCursor(8, 1);
+  while (str.length() < 8)
+    str += " ";
+  lcd.print(str);
+}
+
+
+void setup() {
+  Serial.begin(115200); // Init console
+  Serial.println("Setup started");
+  mySerial.begin(9600); //Init sensor MH-Z19(14)
+  dht.begin();
+
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Initializing...");
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true)
+      delay(10000);
+  }
+
+
+  // attempt to connect to Wifi network:
+  unsigned int attempt = 0;
+  while ( status != WL_CONNECTED) {
+    if (WIFI_MAX_ATTEMPTS_INIT != 0 && attempt > WIFI_MAX_ATTEMPTS_INIT)
+      break;
+    if (attempt >= 65535)
+      attempt = 0;
+    attempt++;
+    lcd.setCursor(0, 0);
+    lcd.print("Connecting " + String(attempt) + "...");
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+    delay(10000);
+  }
+
+  //set google DNS
+  IPAddress googleDNS(8, 8, 8, 8);
+  WiFi.config(WiFi.localIP(), WiFi.gatewayIP(), WiFi.subnetMask(), googleDNS);
+
+  // you're connected now, so print out the data:
+  Serial.println("Connected to network");
+  printCurrentNet();
+  printWifiData();
+
+  //get MAC address
+  byte mac[6];
+  WiFi.macAddress(mac);
+  sprintf(macStr, "%02x:%02x:%02x:%02x:%02x:%02x", mac[5], mac[4], mac[3], mac[2], mac[1], mac[0]);
+
+  Serial.println("Waiting for sensors to init");
+  lcd.setCursor(0, 0);
+  lcd.print("Heating......");
+  delay(10000);
+  Serial.println("Setup finished");
+  Serial.println("");
+  lcd.setCursor(0, 0);
+  lcd.print("Starting.....");
+  lcd.noBacklight();
+}
+
 void loop()
 {
 
   unsigned long currentMillis = millis();
-
   if (currentMillis - previousMillis < INTERVAL)
     return;
   previousMillis = currentMillis;
-  //dns.processNextRequest();
-  //server.handleClient();
   Serial.println("loop started");
+
+  if (errorCount > MAX_DATA_ERRORS)
+  {
+    Serial.println("Too many errors, resetting");
+    lcdPrint(1, "Too many errors.");
+    lcdPrint(3, "Trying to reset.");
+    delay(2000);
+    resetFunc();
+  }
   Serial.println("reading data:");
   int ppm = readCO2();
-
+  bool dataError = false;
   Serial.println("  PPM = " + String(ppm));
 
   if (ppm < 100 || ppm > 6000)
   {
-    Serial.println("  PPM not valid, skipping loop ");
-    return;
+    Serial.println("PPM not valid");
+    lcdPrint(2, "PPM err");
+    dataError = true;
+    lcd.noBacklight();
+  }
+  else
+  {
+    lcdPrint(2, "PPM=" + String(ppm));
+    if (ppm >= 1000)
+      lcd.backlight();
+    else
+      lcd.noBacklight();
   }
   int mem = ESP.getFreeHeap();
   Serial.println("  Free RAM: " + String(mem));
 
   int h = dht.readHumidity();
-
   int t = dht.readTemperature();
 
   Serial.print("  Humidity = ");
@@ -172,34 +217,35 @@ void loop()
   Serial.print(", Temp = ");
   Serial.println(t, 1);
 
-  if (t == 0 || h == 0 || t < 5 || t > 80 || h > 100)
+  if (t < 5 || t > 80 )
   {
-    Serial.println("  temperature\\humidity not valid, skipping loop ");
+    Serial.println("Temperature not valid");
+    lcdPrint(3, "T err");
+    dataError = true;
+  }
+  else
+    lcdPrint(3, "T=" + String(t) + (char)223);
+
+  if (h > 100 || h == 0)
+  {
+    Serial.println("Humidity not valid");
+    lcdPrint(1, "H err");
+    dataError = true;
+  }
+  else
+    lcdPrint(1, "H=" + String(h) + "%");
+
+  if (dataError)
+  {
+    lcdPrint(4, "WiFi skp");
+    Serial.println("Skipping loop");
+    errorCount++;
     return;
   }
+  errorCount = 0;
 
-  lcd.setCursor(0, 0);
-  lcd.print("H=" + String(h) + "%   ");
-  //lcd.setCursor(5, 0);
-  //lcd.print(h, 1);
-
-
-  lcd.setCursor(8, 0);
-  lcd.print("PPM=" + String(ppm) + "  ");
-  if (ppm >= 1000)
-    lcd.backlight();
-  else
-    lcd.noBacklight();
-
-  lcd.setCursor(0, 1);
-  lcd.print("T=" + String(t) + (char)223);
-  lcd.setCursor(8, 1);
-  lcd.print("WiFi *  ");
-
-  // if you get a connection, report back via serial:
   wdt_reset();
   StaticJsonBuffer<200> jsonBuffer;
-
   JsonObject& root = jsonBuffer.createObject();
   root["id"] = 1;
   root["temp"] = t;
@@ -210,23 +256,17 @@ void loop()
   root["SSID"] = WiFi.SSID();
   root["FreeRAM"] = mem;
 
-  String data;
-  root.printTo(data);
-  data = "data=" + data;
 
-  wifiCheckReconnect(ssid, pass);
-  Serial.println("Current net:");
+  wifiCheckReconnect(ssid, pass, WIFI_MAX_ATTEMPTS_SEND);
   printCurrentNet();
 
-  sentOk = sendData(data);
+  lcdPrint(4, "WiFi *");
+  bool sentOk = sendData(root);
   Serial.println("Request sent");
-
-
-  lcd.setCursor(8, 1);
   if (sentOk)
-    lcd.print("WiFi ok!");
+    lcdPrint(4, "WiFi ok!");
   else
-    lcd.print("WiFi err");
+    lcdPrint(4, "WiFi err");
   Serial.println("loop finished");
   Serial.println("");
 }
--- wifi-utils.h
+++ wifi-utils.h
@@ -2,21 +2,26 @@
 WiFiClient client;
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
-void wifiCheckReconnect(char* ssid, char* pass)
+void wifiCheckReconnect(char* ssid, char* pass, int max_attempts = 0)
 {
-  long previousMillis = 0;        // will store last time LED was updated
-  long interval = 10000;           // interval at which to blink (milliseconds)
+  long previousMillis = 0;
+  long interval = 10000;
   unsigned long currentMillis = millis();
+  unsigned int attempt = 0;
   // attempt to connect to Wifi network:
   while ( status != WL_CONNECTED) {
+    if (max_attempts != 0 &&  attempt > max_attempts)
+      break;
+    if (attempt >= 65535)
+      attempt = 0;
+    attempt++;
     wdt_reset();
-    if(currentMillis - previousMillis < interval) 
+    if (currentMillis - previousMillis < interval)
       continue;
-    previousMillis = currentMillis;   
-    //dns.processNextRequest();  
+    previousMillis = currentMillis;
     Serial.print("Wifi not connected. Attempting to connect to SSID ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network:    
+    // Connect to WPA/WPA2 network:
     status = WiFi.begin(ssid, pass);
   }
 }
@@ -26,44 +31,45 @@ void printWifiData() {
   IPAddress ip = WiFi.localIP();
   Serial.print("  IP Address: ");
   Serial.println(ip);
- 
+
   // print your MAC address:
-  byte mac[6];  
+  byte mac[6];
   WiFi.macAddress(mac);
   Serial.print("  MAC address: ");
-  Serial.print(mac[5],HEX);
+  Serial.print(mac[5], HEX);
   Serial.print(":");
-  Serial.print(mac[4],HEX);
+  Serial.print(mac[4], HEX);
   Serial.print(":");
-  Serial.print(mac[3],HEX);
+  Serial.print(mac[3], HEX);
   Serial.print(":");
-  Serial.print(mac[2],HEX);
+  Serial.print(mac[2], HEX);
   Serial.print(":");
-  Serial.print(mac[1],HEX);
+  Serial.print(mac[1], HEX);
   Serial.print(":");
-  Serial.println(mac[0],HEX);
+  Serial.println(mac[0], HEX);
 }
 
 void printCurrentNet() {
+  Serial.println("Current net:");
   // print the SSID of the network you're attached to:
   Serial.print("  SSID: ");
   Serial.println(WiFi.SSID());
 
   // print the MAC address of the router you're attached to:
   /*byte bssid[6];
-  WiFi.BSSID(bssid);    
-  Serial.print("BSSID: ");
-  Serial.print(bssid[5],HEX);
-  Serial.print(":");
-  Serial.print(bssid[4],HEX);
-  Serial.print(":");
-  Serial.print(bssid[3],HEX);
-  Serial.print(":");
-  Serial.print(bssid[2],HEX);
-  Serial.print(":");
-  Serial.print(bssid[1],HEX);
-  Serial.print(":");
-  Serial.println(bssid[0],HEX);*/
+    WiFi.BSSID(bssid);
+    Serial.print("BSSID: ");
+    Serial.print(bssid[5],HEX);
+    Serial.print(":");
+    Serial.print(bssid[4],HEX);
+    Serial.print(":");
+    Serial.print(bssid[3],HEX);
+    Serial.print(":");
+    Serial.print(bssid[2],HEX);
+    Serial.print(":");
+    Serial.print(bssid[1],HEX);
+    Serial.print(":");
+    Serial.println(bssid[0],HEX);*/
 
   // print the received signal strength:
   long rssi = WiFi.RSSI();
@@ -71,8 +77,8 @@ void printCurrentNet() {
   Serial.println(rssi);
 
   // print the encryption type:
- /*byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type:");
-  Serial.println(encryption,HEX);
-  Serial.println();*/
+  /*byte encryption = WiFi.encryptionType();
+    Serial.print("Encryption Type:");
+    Serial.println(encryption,HEX);
+    Serial.println();*/
 }
